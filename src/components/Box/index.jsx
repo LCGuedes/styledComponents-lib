f68d19b1772965ea0288bb50aa8aa73d4919a81f
LCGuedes <lucasmainguedes@gmail.com>
2022-06-15 10:48:33 -0300

    added inline css

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,30 +1,23 @@
 import styled from "styled-components";
 
-const Box = styled.div`
-    width: ${props => props.Width || "600px"};
-    height: ${props => props.Height || "600px"};
-    background-color: ${props => props.backgroundColor || "whitesmoke"};
-    border-radius: ${props => props.borderRadius || "4px"};
-    
-    h1 {
-        color:  ${props => props.fontColor || "red"};
-    }
 
-    h2 {
-        color: blue;
-    }
+const PassingProps = (props,...rest) => {
 
-    h3 {
-        color: yellow;
-    }
-`;
+    //console.log(rest)
 
-export default function BoxComponent({fontColor, Width, Height, backgroundColor, ...rest}) {
+    return {
+        background: props.background,
+        height: props.height,
+        width: props.width
+    };
+};
 
-    console.log(rest.borderRadius)
+const Box = styled.div(props => PassingProps(props))
+
+export default function BoxComponent({...rest}) {
 
     return (
-        <Box fontColor={fontColor} Width={Width} Height={Height} backgroundColor={backgroundColor} etc={rest}>
+        <Box background="green" width="300px" height="500px">
             <h1>teste</h1>
             <h2>teste 2</h2>
             <h3>teste 3</h3>
